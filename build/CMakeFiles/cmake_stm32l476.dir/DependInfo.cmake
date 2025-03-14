35732db54a5099a2bad4dc9aacdf1b59cad9d906
Mateusz Wojtaszek <mateuszwojtaszek@Mac.home>
2025-03-14 13:56:07 +0100

    start projektu

---
+++ build/CMakeFiles/cmake_stm32l476.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/startup_stm32l476xx.s" "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/build/CMakeFiles/cmake_stm32l476.dir/startup_stm32l476xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32L476xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/cmake/stm32cubemx/../../Core/Inc"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/cmake/stm32cubemx/../../Drivers/STM32L4xx_HAL_Driver/Inc"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/cmake/stm32cubemx/../../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32L4xx/Include"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Core/Src/gpio.c" "CMakeFiles/cmake_stm32l476.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Core/Src/gpio.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Core/Src/main.c" "CMakeFiles/cmake_stm32l476.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Core/Src/main.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Core/Src/spi.c" "CMakeFiles/cmake_stm32l476.dir/Core/Src/spi.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Core/Src/spi.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Core/Src/stm32l4xx_hal_msp.c" "CMakeFiles/cmake_stm32l476.dir/Core/Src/stm32l4xx_hal_msp.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Core/Src/stm32l4xx_hal_msp.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Core/Src/stm32l4xx_it.c" "CMakeFiles/cmake_stm32l476.dir/Core/Src/stm32l4xx_it.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Core/Src/stm32l4xx_it.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Core/Src/syscalls.c" "CMakeFiles/cmake_stm32l476.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Core/Src/syscalls.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Core/Src/sysmem.c" "CMakeFiles/cmake_stm32l476.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Core/Src/sysmem.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Core/Src/system_stm32l4xx.c" "CMakeFiles/cmake_stm32l476.dir/Core/Src/system_stm32l4xx.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Core/Src/system_stm32l4xx.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Core/Src/usart.c" "CMakeFiles/cmake_stm32l476.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Core/Src/usart.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_spi_ex.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj.d"
+  "/Users/mateuszwojtaszek/projekty/cmake_stm32l476/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/cmake_stm32l476.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cmake_stm32l476.dir/startup_stm32l476xx.s.d
@@ -0,0 +1,2 @@
+CMakeFiles/cmake_stm32l476.dir/startup_stm32l476xx.s.obj: \
+ /Users/mateuszwojtaszek/projekty/cmake_stm32l476/startup_stm32l476xx.s
